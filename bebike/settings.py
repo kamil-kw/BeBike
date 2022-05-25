29f6a91ef86cb8ae0b5ad84a0510f7211abeeff9
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-05-25 11:54:03 +0000

    Deploy to Heroku via CLI

--- bebike/settings.py
+++ bebike/settings.py
@@ -126,12 +126,21 @@ WSGI_APPLICATION = 'bebike.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
