2b5875ec5cd4b482e9f482a3dc7fe8a54740572e
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-05-27 16:12:56 +0000

    Fixing bug with deployment 7

--- bebike/settings.py
+++ bebike/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://bebike.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['bebike.herokuapp.com', 'localhost']
 
 
 # Application definition
