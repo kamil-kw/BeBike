12c905f0c4014ff5dcf0793ff0a7904ce161f586
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-05-28 08:14:19 +0000

    Sending mails setting

---
+++ bebike/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def handler400(request, exception):
+    """[Handler for Bad Request 400]"""
+    return render(request, "400.html", status=400)
+
+
+def handler403(request, exception):
+    """[Handler forbidden request 403]"""
+    return render(request, "403.html", status=403)
+
+
+def handler404(request, exception):
+    """[Handler for not found request 404]"""
+    return render(request, "404.html", status=404)
+
+
+def handler500(request, *args, **argv):
+    """[Handler for internal server error generic message 500]"""
+    return render(request, "500.html", status=500)
