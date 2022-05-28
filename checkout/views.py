a9bb3b05642f45eba068a94d2832a1c2b373859d
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-05-28 09:12:27 +0000

    Code refactoring

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
