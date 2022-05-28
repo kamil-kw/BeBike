ad535d95d5b113887bce5fcf6ebb62377eb173f6
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-05-28 08:26:23 +0000

    Setting email

--- bebike/settings.py
+++ bebike/settings.py
@@ -26,11 +26,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'b49^^se(ploth7&!y9_qk3@g^#e9d4+mev=uqibswv2n@%bv$g')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = "DEVELOPMENT" in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bebike.herokuapp.com', 'localhost']
 
