50abe414cccdf552d94dc938ba67e995cb7f0869
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-05-30 19:46:13 +0000

    Change model multiply

--- checkout/models.py
+++ checkout/models.py
@@ -55,8 +55,9 @@ class Order(models.Model):
         self.order_total = self.lineitems.aggregate(
                         Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * (
-                            settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+                   if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+                       sdp = settings.STANDARD_DELIVERY_PERCENTAGE
+                       self.delivery_cost = self.order_total * sdp / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
@@ -86,10 +87,11 @@ class OrderLineItem(models.Model):
         Product, null=False, blank=False,
         on_delete=models.CASCADE
     )
-    
+
     product_size = models.CharField(
                 max_length=50, null=True, blank=True
     )
+
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False, editable=False
